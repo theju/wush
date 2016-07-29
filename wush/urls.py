1d49d8bd12e29702732d15502d77447cea2265ad
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-07-29 16:40:36 +0530

    Deprecation warning fix for django 1.10.

--- wush/urls.py
+++ wush/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url
 
 import wush.views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'notify/$', wush.views.send_push_notification, name='send_notification'),
-)
+]
